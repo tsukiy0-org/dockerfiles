047e3a242a558f92e95eb9710aaa85ad464553f2
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-02-28 14:25:39 +1100

    ci: push dotnet-multisdk

--- tasks/ci/build.sh
+++ tasks/ci/build.sh
@@ -4,3 +4,4 @@ set -euo pipefail
 
 echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
 docker build -t tsukiy0/dotnet-multisdk:latest ./src/dotnet-multisdk
+docker push tsukiy0/dotnet-multisdk:latest
