c765be91ceaad1fb3e59a9f9904b8cc504d9ff92
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-02-28 15:31:01 +1100

    ci: deploy node-docker

--- tasks/ci/build.sh
+++ tasks/ci/build.sh
@@ -3,5 +3,9 @@
 set -euo pipefail
 
 echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
+
 docker build -t tsukiy0/dotnet-multisdk:latest ./src/dotnet-multisdk
 docker push tsukiy0/dotnet-multisdk:latest
+
+docker build -t tsukiy0/node-docker:latest ./src/node-docker
+docker push tsukiy0/node-docker:latest
